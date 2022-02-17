ea6803bf0a265e0a87d4bdadd5ddd13e4ccedef5
Mateusz <maticzpl06@gmail.com>
2022-02-17 12:09:29 +0100

    Scroll thumb variable size

--- notifications.lua
+++ notifications.lua
@@ -12,6 +12,133 @@ local json = {}
 local notif = MaticzplNotifications
 local MANAGER = rawget(_G, "MANAGER")    
 
+
+--Ik this code is awful but interface from tpt api is very limiting
+local mouseX = 0
+local mouseY = 0
+local justClicked = false
+local holdingScroll = false
+local scrollLimit = 0
+function MaticzplNotifications.DrawMenuContent()
+    local function hover(x,y,dx,dy)       
+        mouseX = x
+        mouseY = y        
+    end
+    local function click(x,y,button)
+        -- inside window
+        if x > 418 and y > 250 and x < 418 + 193 and y < 250 + 156 and notif.windowOpen then
+            justClicked = true
+
+            if x > 418 and x < 418 + 12 and y > 261 and y < 250 + 156 then
+                holdingScroll = true
+            end
+            return false
+        end
+    end
+    local function unclick(x,y,button,reason)
+        justClicked = false
+        holdingScroll = false
+    end
+
+
+    --Window
+    gfx.fillRect(418,250,193,156,   0,0,0)
+    gfx.drawRect(418,250,193,156,   255,255,255)
+
+
+
+    --Exit button
+    local exitIsHovering = mouseX > 418 and mouseX < 418 + 12 and mouseY > 250 and mouseY < 250 + 12 and notif.windowOpen
+    if exitIsHovering then
+        gfx.fillRect(418,250,12,12, 128,128,128)        
+    end
+    gfx.drawRect(418,250,12,12,     255,255,255)
+    gfx.drawText(418+3,250+2,"X")
+
+    --Scroll Bar
+    local scrollFieldHeight = 250 + 156 - 263
+    local barRatio = math.min(1 - (scrollLimit * -5 / 156),1)
+    local barHeight = math.max(scrollFieldHeight * barRatio,10)
+    if holdingScroll and barHeight < 141 then
+        -- Wolfram alpha saved me here xd
+        notif.scrolled = - ((scrollLimit*(mouseY - (263 + barHeight / 2))) / (barHeight - 141))
+    end
+
+    if notif.scrolled > 0 then
+        notif.scrolled = 0
+    end    
+    if notif.scrolled < scrollLimit then
+        notif.scrolled = scrollLimit
+    end
+    if scrollLimit ~= 0 then      
+        local scrollFraction = notif.scrolled / scrollLimit
+        local barPos = 263 + ((250 + 154 - barHeight - 263) * scrollFraction)
+        gfx.fillRect(420,barPos,8,barHeight, 128,128,128)    
+    else
+        gfx.fillRect(420,263,8,156 - 15, 128,128,128)    
+    end    
+
+    --Vertical line
+    gfx.drawLine(418+11,250,418+11,250 + 155)
+
+    local y = 252 + notif.scrolled * 5
+    local lastTitleY = y
+
+    for i, n in ipairs(notif.notifications) do      
+        local prev = notif.notifications[i-1]
+        
+        local saveID = n.save
+        local title = n.title
+        local msg = n.message
+
+        --Group title
+        if prev == nil or prev.title ~= title then
+            lastTitleY = y
+            if y >= 252 and y <= 250+156 - 10 then         
+                gfx.drawLine(418+12,y - 2,418 + 192,y - 2)     
+                gfx.drawText(418+15,y,title)
+            end
+            local sx,sy = gfx.textSize(title)
+            y = y + sy
+        end
+        --Message
+        if y >= 252 and y <= 250+156 - 10 then         
+            gfx.drawText(418+22,y,msg,200,200,200)    
+        end    
+        local sx,sy = gfx.textSize(msg)
+        y = y + sy
+   
+        local next = notif.notifications[i+1]
+        if next == nil or next.title ~= title then
+            if mouseX > 418 + 12 and mouseX < 418 + 193 and mouseY > lastTitleY and mouseY < y and mouseY > 250 and mouseY < 250 + 156 then
+                local boxY = math.max(lastTitleY-1,251)
+                gfx.drawRect(418 + 12,boxY,193 - 13,math.min(y - lastTitleY-1,boxY - 251 + 154))
+                if justClicked then
+                    sim.loadSave(saveID)
+                end
+            end
+        end
+
+        scrollLimit = -math.max((y - 250 - 154) / 5 - notif.scrolled, 0) 
+    end
+
+
+    event.register(event.mousedown,click)
+    event.register(event.mousemove,hover)
+    event.register(event.mouseup,unclick)
+
+    if exitIsHovering and justClicked then        
+        notif.windowOpen = false
+        notif.notifications = {} 
+        for id, value in pairs(notif.saveCache) do
+            MANAGER.savesetting("MaticzplNotifications",id,notif.SaveToString(value))      
+        end    
+        return false
+    end    
+    justClicked = false
+end
+
+
 -- Request save data from the server
 -- Called automatically every 10 minutes
 function MaticzplNotifications.CheckForChanges()
@@ -116,131 +243,6 @@ function MaticzplNotifications.DrawNotifications()
     gfx.fillCircle(posX,posY,5,5,255,brig,brig)  
     gfx.drawText(posX + 1 -(w / 2),posY + 2 -(h / 2),number,255,255,255)
 end
-
---Ik this code is awful but interface from tpt api is very limiting
-local mouseX = 0
-local mouseY = 0
-local justClicked = false
-local hold = false
-local holdingScroll = false
-local scrollLimit = 0
-function MaticzplNotifications.DrawMenuContent()
-    local function hover(x,y,dx,dy)       
-        mouseX = x
-        mouseY = y        
-    end
-    local function click(x,y,button)
-        -- inside window
-        if x > 418 and y > 250 and x < 418 + 193 and y < 250 + 156 and notif.windowOpen then
-            justClicked = true
-            hold = true
-
-            if x > 418 and x < 418 + 12 and y > 261 and y < 250 + 156 then
-                holdingScroll = true
-            end
-            return false
-        end
-    end
-    local function unclick(x,y,button,reason)
-        justClicked = false
-        holdingScroll = false
-        hold = false        
-    end
-
-
-    --Window
-    gfx.fillRect(418,250,193,156,   0,0,0)
-    gfx.drawRect(418,250,193,156,   255,255,255)
-
-    --Exit button
-    local exitIsHovering = mouseX > 418 and mouseX < 418 + 12 and mouseY > 250 and mouseY < 250 + 12 and notif.windowOpen
-    if exitIsHovering then
-        gfx.fillRect(418,250,12,12, 128,128,128)        
-    end
-    gfx.drawRect(418,250,12,12,     255,255,255)
-    gfx.drawText(418+3,250+2,"X")
-
-    --Scroll Bar
-    local barHeight = 12 
-    if holdingScroll then
-        -- Wolfram alpha saved me here xd
-        notif.scrolled = - ((scrollLimit*(mouseY - 269)) / (barHeight - 141))
-    end
-    if notif.scrolled > 0 then
-        notif.scrolled = 0
-    end    
-    if notif.scrolled < scrollLimit then
-        notif.scrolled = scrollLimit
-    end
-    if scrollLimit ~= 0 then      
-        local scrollFraction = notif.scrolled / scrollLimit
-        local barPos = 263 + ((250 + 154 - barHeight - 263) * scrollFraction)
-        gfx.fillRect(420,barPos,8,barHeight, 128,128,128)    
-    end    
-
-    --Vertical line
-    gfx.drawLine(418+11,250,418+11,250 + 155)
-
-    local y = 252 + notif.scrolled * 5
-    local lastTitleY = y
-
-    for i, n in ipairs(notif.notifications) do      
-        local prev = notif.notifications[i-1]
-        
-        local saveID = n.save
-        local title = n.title
-        local msg = n.message
-
-        --Group title
-        if prev == nil or prev.title ~= title then
-            lastTitleY = y
-            if y >= 252 then         
-                gfx.drawLine(418+12,y - 2,418 + 192,y - 2)
-            end
-            if y >= 252 then         
-                gfx.drawText(418+15,y,title)
-            end
-            local sx,sy = gfx.textSize(title)
-            y = y + sy
-        end
-        --Message
-        if y >= 252 then         
-            gfx.drawText(418+22,y,msg,200,200,200)    
-        end    
-        local sx,sy = gfx.textSize(msg)
-        y = y + sy
-   
-        local next = notif.notifications[i+1]
-        if next == nil or next.title ~= title then
-            if mouseX > 418 + 12 and mouseX < 418 + 193 and mouseY > lastTitleY and mouseY < y then
-                if lastTitleY >= 252 then         
-                    gfx.drawRect(418 + 12,lastTitleY-1,193 - 13,y - lastTitleY-1)
-                    if justClicked then
-                        sim.loadSave(saveID)
-                    end
-                end
-            end
-        end
-
-        scrollLimit = -((y - 252) / 5 - notif.scrolled )
-    end
-
-
-    event.register(event.mousedown,click)
-    event.register(event.mousemove,hover)
-    event.register(event.mouseup,unclick)
-
-    if exitIsHovering and justClicked then        
-        notif.windowOpen = false
-        notif.notifications = {} 
-        for id, value in pairs(notif.saveCache) do
-            MANAGER.savesetting("MaticzplNotifications",id,notif.SaveToString(value))      
-        end    
-        return false
-    end    
-    justClicked = false
-end
-
 
 -- Used for saving current state of saves
 function MaticzplNotifications.SaveToString(save)
